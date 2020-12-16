9063887978fd9c304364d1780ff6fe0f917616bf
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-16 10:10:29 -1000

    Added Xcode project.

---
+++ Sport Riot/Sport RiotTests/Sport_RiotTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sport_RiotTests.swift
+//  Sport RiotTests
+//
+//  Created by Jesse Ruiz on 12/16/20.
+//
+
+import XCTest
+@testable import Sport_Riot
+
+class Sport_RiotTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
