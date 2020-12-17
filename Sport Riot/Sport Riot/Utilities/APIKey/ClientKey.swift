2444c7656248a6775c0da8eb1daee05a5b82bed5
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-17 08:39:15 -1000

    Added client key file and template file for network call.

---
+++ Sport Riot/Sport Riot/Utilities/APIKey/ClientKey.swift
@@ -0,0 +1,12 @@
+//
+//  ClientKey.swift
+//  Sport Riot
+//
+//  Created by Jesse Ruiz on 12/17/20.
+//
+
+import Foundation
+
+struct ClientKey {
+    static var clientKey = "client_id=MjE0NDQ0MDJ8MTYwODE1MTc4OS45MDA0OTc0"
+}
---
+++ Sport Riot/Sport Riot/Utilities/APIKey/ClientKeyTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  ClientKeyTemplate.swift
+//  Sport Riot
+//
+//  Created by Jesse Ruiz on 12/17/20.
+//
+
+/*
+ Please make sure to add your own key below and uncomment the code.
+ */
+
+import Foundation
+
+/*
+struct ClientKey {
+    static var clientKey = ""
+}
+*/
